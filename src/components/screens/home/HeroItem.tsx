4ea9c07cd9435f8cde1befbee2418c632521887f
d1m0n4ik <svyryd2012@gmail.com>
2023-07-13 20:01:22 +0300

    hero animation

--- src/components/screens/home/HeroItem.tsx
+++ src/components/screens/home/HeroItem.tsx
@@ -1,25 +1,28 @@
 import { FC } from 'react'
 import { IResult } from '../../../interfaces/IUpcomingMovie'
 import { getOriginalImagePath, getWidth500ImagePath } from '../../../store/api'
+import style from './Home.module.scss'
+
 type HeroItemPropsType = {
 	item: IResult
 }
+
 const HeroItem: FC<HeroItemPropsType> = ({ item }) => {
 	const background = getOriginalImagePath(item.backdrop_path)
 	return (
 		<div
-			className="w-full h-full flex after:content-['*'] after:bg-black'"
+			className={style.heroItem}
 			style={{
-				backgroundImage: `url(${background})`,
+				background: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.8)), url(${background})`,
 				backgroundPosition: 'center',
 				backgroundSize: 'cover',
 			}}>
-			<div className='w-1/2 -full flex items-center flex-col justify-center p-4'>
-				<div>{item.title}</div>
-				<div>{item.overview}</div>
+			<div className={`${style.heroItemCol}`}>
+				<div className='text-xl md:text-4xl pb-4'>{item.title}</div>
+				<div className='w-full md:w-3/4 text-base  md:text-xl text-justify indent-4 p-2'>{item.overview}</div>
 			</div>
-			<div className='w-1/2 h-full flex items-center justify-center'>
-				<img className='w-80' src={getWidth500ImagePath(item.poster_path)} />
+			<div className={`${style.heroItemCol}`}>
+				<img className='w-44 sm:56  md:w-80' src={getWidth500ImagePath(item.poster_path)} />
 			</div>
 		</div>
 	)
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
-import './index.sass'
+import './index.scss'
 import store from './store/store.ts'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
