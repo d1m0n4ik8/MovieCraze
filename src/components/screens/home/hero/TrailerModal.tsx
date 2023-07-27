06893e821f75613539c17082fe22e54a20d4649a
d1m0n4ik <svyryd2012@gmail.com>
2023-07-27 13:19:59 +0300

    update trailer modal

--- src/components/screens/home/hero/TrailerModal.tsx
+++ src/components/screens/home/hero/TrailerModal.tsx
@@ -1,6 +1,5 @@
 import { FC, SetStateAction } from 'react'
 import { useGetMovieVideoQuery } from '../../../../store/api'
-import Modal from '../../../ui/modal/Modal'
 
 type trailerPropsType = {
 	modalIsOpened: boolean
@@ -11,17 +10,24 @@ type trailerPropsType = {
 const TrailerModal: FC<trailerPropsType> = ({ modalIsOpened, setModalIsOpened, trailerId }) => {
 	const { data, isLoading } = useGetMovieVideoQuery(trailerId)
 	return (
-		<Modal modalIsOpened={modalIsOpened} setModalIsOpened={setModalIsOpened}>
-			{isLoading ? (
-				<div>Loading...</div>
-			) : (
-				<iframe
-					src={`https://www.youtube.com/embed/${data?.results[0].key}`}
-					width='100%'
-					height='500px'
-					title='trailer'></iframe>
+		<>
+			{!isLoading && modalIsOpened && (
+				<div
+					className='fixed w-full h-full bg-gray-950 bg-opacity-50 top-0 left-0 flex justify-center items-center cursor-pointer overflow-hidden z-10'
+					onClick={() => setModalIsOpened(false)}>
+					<div
+						className='bg-black bg-opacity-75 cursor-default w-3/4 rounded-2xl shadow-lg shadow-teal-400 p-8'
+						onClick={e => e.stopPropagation()}>
+						<iframe
+							src={`https://www.youtube.com/embed/${data?.results[0].key}`}
+							width='100%'
+							height='500px'
+							title='trailer'
+						/>
+					</div>
+				</div>
 			)}
-		</Modal>
+		</>
 	)
 }
 
